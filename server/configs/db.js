5dbe42db93a8108bd07f6c3ce656524a7d83ece2
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2026-03-07 12:05:17 +0000

    config.db updated

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,12 +1,19 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
-    try {
-        mongoose.connection.on('connected', () => console.log("Database Connected"));
-        await mongoose.connect(process.env.MONGODB_URI);
-    } catch (error) {
-        console.error(error.message);
-    }
-}
+  try {
+    mongoose.connection.on('connected', () =>
+      console.log('Database Connected'),
+    );
+    await mongoose.connect(process.env.MONGODB_URI, {
+      maxPoolSize: 10, // Máximo 10 conexões por instância
+      minPoolSize: 2, // Mínimo 2 prontas
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
 export default connectDB;
